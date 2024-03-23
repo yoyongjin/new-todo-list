ac9bc4c55d1d52b6a2d87bf599821f34e91f0453
yoyongjin <yongjinalize@gmail.com>
2024-03-23 22:10:13 +0900

    ✨  Add: AddTodoForm에 addTodo 기능 추가

--- src/components/molecules/AddTodoForm.tsx
+++ src/components/molecules/AddTodoForm.tsx
@@ -1,15 +1,31 @@
+import { useState } from "react";
+import { uid } from "react-uid";
 import styled from "styled-components";
-import TextInput from "../atoms/TextInput";
+import { useTodoStore } from "../../store/useTodoStore";
 import TextButton from "../atoms/TextButton";
+import TextInput from "../atoms/TextInput";
 
 const Container = styled.form`
   display: flex;
 `;
 function AddTodoForm() {
+  const [taskText, setTaskText] = useState<string>("");
+  const addTodo = useTodoStore((state) => state.addTodo);
+  const todos = useTodoStore((state) => state.todos);
+
+  console.log(todos);
+
   return (
     <Container>
-      <TextInput />
-      <TextButton />
+      <TextInput
+        onChange={(e) => setTaskText(e.target.value)}
+        value={taskText}
+      />
+      <TextButton
+        onClick={() =>
+          addTodo({ id: uid(taskText), task: taskText, isCompleted: false })
+        }
+      />
     </Container>
   );
 }
