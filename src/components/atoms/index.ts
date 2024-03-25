0ed1e35d4db55624bf39808387839449e3bb75fa
yoyongjin <yongjinalize@gmail.com>
2024-03-25 11:36:07 +0900

    🎨 Refactor: atomic design에 네임스페이스 추가
     - styled-components와 atomic design pattern과의 구분을 하기 위한 네임스페이스

---
+++ src/components/atoms/index.ts
@@ -0,0 +1,11 @@
+import Label from "./Label";
+import TextButton from "./TextButton";
+import TextInput from "./TextInput";
+
+const Atoms = {
+  TextButton,
+  TextInput,
+  Label,
+};
+
+export default Atoms;
---
+++ src/components/molecules/index.ts
@@ -0,0 +1,11 @@
+import AddTodoForm from "./AddTodoForm";
+import LabeledInput from "./LabeledInput";
+import TodoItem from "./TodoItem";
+
+const Molecule = {
+  AddTodoForm,
+  TodoItem,
+  LabeledInput,
+};
+
+export default Molecule;
