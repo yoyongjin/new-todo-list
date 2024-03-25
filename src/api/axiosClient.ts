2108550c0caec482a792c3df98163d4033b7d9e1
yoyongjin <yongjinalize@gmail.com>
2024-03-25 11:09:51 +0900

    🔧 Add: .env에서 BASE_URL 설정 및 axios.create 설정 추가

--- src/api/axiosClient.ts
+++ src/api/axiosClient.ts
@@ -1,11 +1,12 @@
 import axios from "axios";
 
 const axiosClient = axios.create({
-  baseURL: "https://your-api-domain.com/api", // API의 기본 URL
+  baseURL: import.meta.env.BASE_URL, // API의 기본 URL
   headers: {
     "Content-Type": "application/json",
     // 필요한 경우 추가 헤더를 설정할 수 있습니다.
   },
+  timeout: 5000,
 });
 
 export default axiosClient;
