7a731330d7e1af33d3098b2675e55b5588f88f49
yoyongjin <yongjinalize@gmail.com>
2024-03-25 16:59:24 +0900

    ✨ Feat: 회원가입/로그인 로직 구현

--- src/components/organisms/AuthForm.tsx
+++ src/components/organisms/AuthForm.tsx
@@ -1,20 +1,89 @@
+import Molecules from "@components/molecules";
+import { DevTool } from "@hookform/devtools";
+import axios from "axios";
+import { FieldValues, SubmitHandler, useForm } from "react-hook-form";
 import styled from "styled-components";
-import Molecules from "../molecules";
 import Atoms from "../atoms";
+import { useCallback, useState } from "react";
 
 export interface Props {}
 
-const Container = styled.div`
+export interface IAuthFormData {
+  email: string;
+  password: string;
+}
+
+const Form = styled.form`
   display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  gap: 30px;
+  background-color: #ddd;
+  padding: 20px;
 `;
 
 function AuthForm() {
+  const [authMode, setAuthMode] = useState<"signup" | "login">("signup");
+  const { control, register, handleSubmit } = useForm();
+
+  const onSubmit: SubmitHandler<FieldValues> = useCallback(
+    async (data) => {
+      console.log(data);
+      switch (authMode) {
+        case "signup": {
+          try {
+            // const response = await axiosClient.post("auth/signup", data);
+            const response = await axios.post(
+              "http://192.168.20.130:4000/api/auth/signup",
+              data
+            );
+
+            console.log(response.data);
+          } catch (error) {
+            console.error(error);
+          }
+
+          break;
+        }
+        case "login": {
+          try {
+            // const response = await axiosClient.post("auth/login", data);
+            const response = await axios.post(
+              "http://192.168.20.130:4000/api/auth/login",
+              data
+            );
+
+            console.log(response.data);
+          } catch (error) {
+            console.error(error);
+          }
+        }
+      }
+    },
+    [authMode]
+  );
+
   return (
-    <Container>
-      <Molecules.LabeledInput labelConfig={{ children: "이메일" }} />
-      <Molecules.LabeledInput labelConfig={{ children: "비밀번호" }} />
-      <Atoms.TextButton>로그인</Atoms.TextButton>
-    </Container>
+    <Form onSubmit={handleSubmit(onSubmit)}>
+      <Molecules.LabeledInput
+        labelConfig={{ children: "이메일" }}
+        inputConfig={{ name: "email" }}
+        register={register("email")}
+      />
+      <Molecules.LabeledInput
+        labelConfig={{ children: "비밀번호" }}
+        inputConfig={{ name: "password" }}
+        register={register("password")}
+      />
+      <Atoms.TextButton onClick={() => setAuthMode("signup")}>
+        회원가입
+      </Atoms.TextButton>
+      <Atoms.TextButton onClick={() => setAuthMode("login")}>
+        로그인
+      </Atoms.TextButton>
+      <DevTool control={control} />
+    </Form>
   );
 }
 
--- src/pages/auth/index.tsx
+++ src/pages/auth/index.tsx
@@ -1,13 +1,23 @@
+import Organisms from "@components/organisms";
 import styled from "styled-components";
 
 export interface Props {}
 
 const Container = styled.div`
   display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  height: 100vh;
+  background-color: #ccc;
 `;
 
 function AuthPage() {
-  return <Container>AuthPage</Container>;
+  return (
+    <Container>
+      <Organisms.AuthForm />
+    </Container>
+  );
 }
 
 export default AuthPage;
